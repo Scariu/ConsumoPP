33f3893aff79b34563371ad3fa0a0f546f935b7b
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-28 20:58:42 -0400

    Se implementa DAO (interface ElementoDAO).

--- app/src/main/java/com/example/consumopp/data/local/ElementoDAO.kt
+++ app/src/main/java/com/example/consumopp/data/local/ElementoDAO.kt
@@ -1,4 +1,15 @@
 package com.example.consumopp.data.local
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface ElementoDAO {
+    @Insert
+    suspend fun insertElemento(elemento: Elemento)
+
+    @Query("SELECT * FROM table_elementos order by id ASC")
+    fun getTodosElementos(): LiveData<List<Elemento>>
 }
