f2f95449c9a67f5f34aa56e166852e2d347c6399
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-28 20:54:02 -0400

    Se implementa Entity (class Elemento).

--- app/src/main/java/com/example/consumopp/data/local/Elemento.kt
+++ app/src/main/java/com/example/consumopp/data/local/Elemento.kt
@@ -1,4 +1,9 @@
 package com.example.consumopp.data.local
 
-class Elemento {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "table_elementos")
+class Elemento (private val nombre: String, private val precio: Long, private val cantidad: Int){
+    @PrimaryKey (autoGenerate = true) var id: Long = 0
 }
