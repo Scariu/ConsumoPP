dca749964e457633df8b3a82fb1b58be2434e5a7
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-31 20:08:41 -0400

    En FirstFragmentAgregar se agrega función a btnGuardar , el cual guarda los datos ingresados.

--- app/src/main/java/com/example/consumopp/presentation/FirstFragmentAgregar.kt
+++ app/src/main/java/com/example/consumopp/presentation/FirstFragmentAgregar.kt
@@ -5,13 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.consumopp.R
+import com.example.consumopp.data.local.Elemento
+import com.example.consumopp.databinding.FragmentFirstAgregarBinding
+
 class FirstFragmentAgregar : Fragment() {
+    lateinit var binding: FragmentFirstAgregarBinding
+    private val viewModel: ElementoViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first_agregar, container, false)
+        binding = FragmentFirstAgregarBinding.inflate(layoutInflater)
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners() {
+        binding.btnGuardar.setOnClickListener {
+            getData()
+        }
+    }
+
+    private fun getData() {
+        val nombre = binding.etNombre.text.toString()
+        val precio = binding.etPrecio.text.toString().toLong()
+        val cantidad = binding.etCantidad.text.toString().toInt()
+
+        viewModel.insertItem(nombre, precio, cantidad)
     }
 }
