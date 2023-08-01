bc43df37e827fd425520178e261461c990c6b449
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-31 21:31:44 -0400

    Se implementa class Adapter.

--- app/src/main/java/com/example/consumopp/presentation/Adapter.kt
+++ app/src/main/java/com/example/consumopp/presentation/Adapter.kt
@@ -1,4 +1,39 @@
 package com.example.consumopp.presentation
 
-class Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.consumopp.data.local.Elemento
+import com.example.consumopp.databinding.ItemBinding
+
+class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
+    private val listItem = mutableListOf<Elemento>()
+    lateinit var binding: ItemBinding
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = ItemBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItem[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return listItem.size
+    }
+
+
+    class ViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Elemento) {
+            binding.tvItemNombre.text = item.nombre
+            binding.tvItemPrecio.text = item.precio.toString()
+            binding.tvItemCantidad.text = item.cantidad.toString()
+            binding.tvTotalCard.text = (item.precio * item.cantidad).toString()
+
+        }
+
+    }
 }
